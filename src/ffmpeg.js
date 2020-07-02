0cf2f8f1e330016059769f42fa076085b66b06da
Nguyen Van Bac <bacnv2@hocmai.vn>
2020-07-02 17:22:09 +0700

    update

--- src/ffmpeg.js
+++ src/ffmpeg.js
@@ -3,13 +3,13 @@ const ffmpegInstaller = require('@ffmpeg-installer/ffmpeg');
 
 ffmpeg.setFfmpegPath(ffmpegInstaller.path);
 
-ffmpeg('videos/video.mp4', { timeout: 432000 }).addOptions([
+ffmpeg('videos/gia-dinh.mp4', { timeout: 432000 }).addOptions([
     '-profile:v baseline',
     '-level 3.0',
     '-start_number 0',
-    '-hls_time 10',
+    '-hls_time 5',
     '-hls_list_size 0',
     '-f hls'
-]).output('videos/output.m3u8').on('end', () => {
+]).output('videos/gia-dinh/index.m3u8').on('end', () => {
     console.log('end');
 }).run();
