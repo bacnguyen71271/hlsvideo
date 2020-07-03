f098d5c2f73ff564da265cdbe817020e5f6b4bc4
Nguyen Van Bac <bacnv2@hocmai.vn>
2020-07-03 13:46:22 +0700

    update

--- src/app.js
+++ src/app.js
@@ -4,18 +4,9 @@ const hls = require('hls-server');
 const cors = require('cors');
 
 
-const allowedOrigins = ['video.edusing123.com',
-    'edusing123.com'];
 app.use(cors({
-    origin: function(origin, callback){
-        if(!origin) return callback(null, true);
-        if(allowedOrigins.indexOf(origin) === -1){
-            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    }
-    
+    origin: 'http://edusing123.com',
+    optionsSuccessStatus: 200
 }));
 
 
