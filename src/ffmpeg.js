ff69da21098fda7ef614f88294516b3d8f160fdb
Bac Nguyen <bac.nguyen.71271@gmail.com>
2020-07-05 00:00:07 +0700

    update

--- src/ffmpeg.js
+++ src/ffmpeg.js
@@ -3,13 +3,13 @@ const ffmpegInstaller = require('@ffmpeg-installer/ffmpeg');
 
 ffmpeg.setFfmpegPath(ffmpegInstaller.path);
 
-ffmpeg('videos/phong-khach.mp4', { timeout: 432000 }).addOptions([
+ffmpeg('videos/thu-cung.mp4', { timeout: 432000 }).addOptions([
     '-profile:v baseline',
     '-level 3.0',
     '-start_number 0',
     '-hls_time 5',
     '-hls_list_size 0',
     '-f hls'
-]).output('videos/phong-khach/index.m3u8').on('end', () => {
+]).output('videos/thu-cung/index.m3u8').on('end', () => {
     console.log('end');
 }).run();
