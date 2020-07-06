0ffb33735a927f6e7bd5d570b7041fb0dd00528c
Bac Nguyen <bac.nguyen.71271@gmail.com>
2020-07-07 00:49:18 +0700

    update

---
+++ src/cv.js
@@ -0,0 +1,16 @@
+const cv = require('fluent-ffmpeg');
+const ffmpegInstaller = require('@ffmpeg-installer/ffmpeg');
+
+cv.setFfmpegPath(ffmpegInstaller.path);
+
+cv('videos/di-sieu-thi.mp4', { timeout: 432000 }).addOptions([
+    '-profile:v baseline',
+    '-level 3.0',
+    '-start_number 0',
+    '-hls_time 5',
+    '-hls_key_info_file enc.keyinfo',
+    '-hls_list_size 0',
+    '-f hls'
+]).output('videos/di-sieu-thi/index.m3u8').on('end', () => {
+    console.log('end');
+}).run();
