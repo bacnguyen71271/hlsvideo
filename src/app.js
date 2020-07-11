7e3ebf9394814aa76e730f49628e943f1bcbea8e
Bac Nguyen <bac.nguyen.71271@gmail.com>
2020-07-11 09:02:33 +0700

    update

--- src/app.js
+++ src/app.js
@@ -19,25 +19,29 @@ app.get('/enc.key', (req, res) => {
 });
 
 const server = app.listen(3100);
+app.use(express.static('videos'));
 
+/*
 new hls(server, {
     provider: {
         exists: (req, cb) => {
             const ext = req.url.split('.').pop();
 
-            // if (ext !== 'm3u8' && ext !== 'ts') {
-            //     return cb(null, true);
-            // }
+            if (ext !== 'm3u8' && ext !== 'ts') {
+                return cb(null, true);
+            }
 
             fs.access(__dirname + req.url, fs.constants.F_OK, function (err) {
                 if (err) {
                     console.log('File not exist');
                     return cb(null, false);
                 }
+                console.log(__dirname + req.url);
                 cb(null, true);
             });
         },
         getManifestStream: (req, cb) => {
+            console.log(__dirname + req.url);
             const stream = fs.createReadStream(__dirname + req.url);
             cb(null, stream);
         },
@@ -47,3 +51,4 @@ new hls(server, {
         }
     }
 });
+*/
